f5c52e26203f201b3926decee8ab47765d282e0d
zaid25akhter <zaid25akhter@gmail.com>
2025-01-25 20:37:48 +0530

    bean

---
+++ sbp1/app/src/main/java/org/example/service/OrderService.java
@@ -0,0 +1,11 @@
+package org.example.service;
+
+import org.example.bean.UserConfig;
+
+public class OrderService {
+
+    //factory method
+    public static OrderService createInstance(UserService userService, UserConfig userConfig) {
+        return null;
+    }
+}
